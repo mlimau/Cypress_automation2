8f50959e1faf484b1c8d7df88c9053f048d46600
MlimaU <129126676+mlimau@users.noreply.github.com>
2024-04-03 16:17:49 -0400

    added: autoFill(with dropdown using debugger), mochawesome report added+description in autoFill test

--- pom/autoFill.ts
+++ pom/autoFill.ts
@@ -7,11 +7,11 @@
         const obj = {
             Y :'Yellow'
         }
-        cy.get(this.inputField).eq(0).type(Object.keys(obj)[0])//enter y
+        cy.get(this.inputField).eq(0).type(Object.keys(obj)[0])//enter y into first fiels (selector shows 2 fields)
         //cy.pause()
-        cy.contains(this.dropDown,Object.values(obj)[0]).click()
+        cy.contains(this.dropDown,Object.values(obj)[0]).click()//click on dropdown option where appears 'Yellow' to choose it and put into the field
         //cy.pause()
-        cy.get(this.result).should('have.text', Object.values(obj)[0])
+        cy.get(this.result).should('have.text', Object.values(obj)[0])//'Yellow' showld be in the field
 
 
    }
@@ -22,3 +22,10 @@ export const autoFill = new AutoFill()
  // Cypress.on('uncaught:exception', (err, runnable) => {
  //  return false
  // })показывал uncaught:exception error, когда я прописала настройку всё стало проходить
+
+
+ //we were making the test using in console (devtools): setTimeout(() => {debugger;}, 5000);
+ //so, enter setTimeout into console, during 5 sec put your letter into the field and wait the
+ //appearing options with words, then inspect it (debugger allows it),
+ // then click on debugger (small window on the top), then choose certain
+ // word in dropdown and inspect it with your word already(it gonna be in Elements)
