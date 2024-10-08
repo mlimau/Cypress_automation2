779859220dfb2971272850e32bcdbfbf969f9177
mlima <129126676+mlimau@users.noreply.github.com>
2024-10-08 18:18:34 -0400

    Update loadDelay.cy.ts

--- cypress/e2e/loadDelay.cy.ts
+++ cypress/e2e/loadDelay.cy.ts
@@ -4,9 +4,9 @@ describe('LOAD DELAY', () => {
     });
 
     it('load delay emulate', ()=> {
-        cy.visit('http://uitestingplayground.com/home', { timeout: 3000})// //здесь настраиваем timeout локально так как глобально Мы указываем в cypress.config.ts
+        cy.visit('http://uitestingplayground.com/home', { timeout: 3000})
         cy.get('[href="/loaddelay"]').click()//selectot for link with href
-        //cy.wait(3000)
+       
         cy.contains('button', 'Button Appearing After Delay')
     })
 })
